4f0ce69c2d12b8a97615f225e830494f6475cd07
Debjyoti Paul <debjyoti.paul@grorapid.com>
2025-02-14 11:18:07 +0530

    Firebase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:orchestrate/core/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
